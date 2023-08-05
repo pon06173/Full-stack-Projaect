3eb9ff040245e2b9518230bf51e8eb29be8b5119
pon06173 <pon06173@gmail.com>
2023-08-06 01:46:18 +0900

    2

--- 8M/2023-08-03/test03/my-app/src/App.js
+++ 8M/2023-08-03/test03/my-app/src/App.js
@@ -1,40 +1,39 @@
-import './App.css';
-
-
+import "./App.css";
 
 function App() {
-
-  let title = "Todo List"
+  let title = "버튼 클릭!";
   let elementList = [
-    <button key={0} onClick={btnEventHandler}>1.hello</button>,
-    <button key={1} onClick={btnEventHandler}>2.world</button>,
-    <button key={2} onClick={btnEventHandler}>3.orange</button>,
-    <button key={3} onClick={btnEventHandler}>4.tomato</button>
-  ]
+    <button key={0} onClick={btnEventHandler2}>
+      1.hello
+    </button>,
+    <button key={1} onClick={btnEventHandler2}>
+      2.world
+    </button>,
+    <button key={2} onClick={btnEventHandler2}>
+      3.orange
+    </button>,
+    <button key={3} onClick={btnEventHandler2}>
+      4.tomato
+    </button>,
+  ];
 
   function btnEventHandler(e) {
-    if(e.target.innerText === elementList[0].props.children){
-      console.log("첫번째 버튼: ", e.target.elementList[0])
-    }
-      
-    
+    console.log("Button clicked!");
+  }
 
-    if(e.target.dataset.msg){
-      console.log("Message: ", title)
-    } else {
-      console.log("Clicked: ", e.target.innerText)
-    }
+  function btnEventHandler2(e) {
+    console.log(e.target.innerText);
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{title} App</h1>
-        <button data-name="msg" onClick={btnEventHandler}>메세지 출력</button>
+        <h1>{title}</h1>
+        <button data-name="msg" onClick={btnEventHandler}>
+          버튼입니다
+        </button>
       </header>
-      <div>
-    {elementList}
-      </div>
+      <div>{elementList}</div>
     </div>
   );
 }
--- 8M/2023-08-03/test04/my-app/src/App.js
+++ 8M/2023-08-03/test04/my-app/src/App.js
@@ -1,39 +1,43 @@
-import './App.css';
-import {useState} from 'react'
-
-
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [inputValue, setInputValue] = useState("")
-  const [fruitList, setFruitList] = useState(["사과","오렌지","딸기"])
-  let list = fruitList.map((fruit, i)=>{
-    return <li key={i}>
-      {fruit}
-    </li>
-  })
+  let title = "랜덤 숫자";
+  const [inputValue, setInputValue] = useState("");
+  const [number, setNumber] = useState(0);
+  const [colorStyle, setColorStyle] = useState({ color: "red" });
 
-  function saveBtnHandler(e) {
-    setFruitList([...fruitList, inputValue])
-    setInputValue("")
+  function numBtnHandler() {
+    //난수 발생
+    let number = Math.floor(Math.random() * 401) - 200;
+    setNumber(number);
+    if (number <= 0) {
+      setColorStyle({ color: "red" });
+    } else {
+      setColorStyle({ color: "blue" });
+    }
   }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>과일 목록</h1>   
-    </header>
-    <div>
-      <p>
-        과일: <input value={inputValue} onChange={(e)=>setInputValue(e.target.value)}/>
-        <button onClick={saveBtnHandler}>저장</button>
-      </p>
-      <div>
-        <ul>
-          {list}
-        </ul>
+        <h1>{title} App</h1>
+        내용입력:{" "}
+        <input
+          value={inputValue}
+          onChange={(e) => {
+            //inputValue = e.target.value;
+            setInputValue(e.target.value);
+            console.log(inputValue);
+          }}
+        />
+        <div>내용: {inputValue}</div>
+        {/* <div><button onClick={numBtnHandler}>숫자 생성</button></div> */}
+      </header>
+      <div className="App-content">
+        {/* <p style={colorStyle}>숫자 : {number}</p> */}
       </div>
     </div>
-    </div>
-    
   );
 }
 
