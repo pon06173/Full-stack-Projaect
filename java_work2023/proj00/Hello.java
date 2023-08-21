77145ddca372fe39919b271959436ae2139a78ca
pon06173 <pon06173@gmail.com>
2023-08-21 16:04:26 +0900

    1

---
+++ java_work2023/proj00/Hello.java
@@ -0,0 +1,6 @@
+// Class이름은 파일명과 동일하게
+public class Hello{
+	public static void main(String[] args) {
+		System.out.println("Hello java world!");
+	}
+}
---
+++ java_work2023/proj01/src/proj04/Ch04Ex01.java
@@ -0,0 +1,82 @@
+package proj04;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Phone {
+   private String name;
+   private String phone;
+   
+   public Phone() {
+      // Default Constructor
+   }
+   
+   // 생성자 오버로딩
+public Phone(String name, String phone) {
+	//super(); 상속 받지 않으니깐 부모의 생성자 호출은 삭제 해야함.
+	this.name = name;
+	this.phone = phone;
+}
+
+// setters/getters
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getPhone() {
+	return phone;
+}
+
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+
+@Override//phone의 내용 return
+public String toString() {
+	return "[name=" + name + ", phone=" + phone + "]";
+}
+   
+   
+   
+}
+
+public class Ch04Ex01 {
+	//전화번호 10개를 phoneArr에 저장하고
+	//배열이 어렵다면 ArrayList로 사용해도 됩니다.
+	//입력, 출력, 검색, 수정, 삭제 되도록 구현하세요.
+	
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      Phone[] phoneArr = new Phone[10];
+      
+      for(int i=0; i<phoneArr.length; i++) {
+          System.out.print("이름 입력: ");
+          String name = scan.next();
+          System.out.print("전화 번호 입력");
+          String phone = scan.next();
+          Phone person = new Phone(name, phone);
+          phoneArr[i] = person;
+       }
+
+      
+//      System.out.print("이름 입력: ");
+//      String name = scan.next();
+//      System.out.print("전화 번호 입력: ");
+//      String phone = scan.next();
+//      Phone person = new Phone(name, phone);
+//      phoneArr[0] = person;
+//      
+//      System.out.print("이름 입력: ");
+//      name = scan.next();
+//      System.out.print("전화 번호 입력: ");
+//      phone = scan.next();
+//      person = new Phone(name, phone);
+//      phoneArr[1] = person;
+      
+      System.out.println(Arrays.toString(phoneArr));
+   }
+}
